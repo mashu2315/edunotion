a810155c779eea6e8e5d2f073e0a2245c6a6c895
mashu2315 <mauryaashutosh1983@gmail.com>
2025-12-11 13:01:06 +0530

    update import

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 // updating cors 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: `${process.env.FRONTEND_URL}`, 
   credentials: true,
    allowedHeaders: ["Content-Type", "Authorization"],
     methods: "GET,POST,PUT,DELETE",
